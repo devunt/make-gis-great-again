b1cf357c5a914534cfae4c5089e281085d176919
AHOHNMYC <Zpa2tR5bMrMDEEBpPY6MdTIhEepg7@ya.ru>
2018-02-20 18:13:55 +0300

    Add "Other resolution" button
    * ! Localisation mechanism rewritten to support not only one string.
    * Added global variable `linkToCurrentImage` to store obvuous link to current image
    * Add "Other resolution" button.
    It takes a time to load a page because of loading other page in background.
    Maybe some kind of splashscreen can help user recognise that script is working.
    
    Tested on Tampermonkey+Chromium.

--- gis-vib.user.js
+++ gis-vib.user.js
@@ -1,61 +1,116 @@
 // ==UserScript==
 // @name            Google Search "View Image" Button
 // @name:ru         Google Search кнопка "Показать в полном размере"
+// @name:sl         Gumb "Ogled slike" na Google Slikah
+// @name:lt         Google paieškos mygtukas "Rodyti vaizdą"
 // @namespace       https://github.com/devunt/make-gis-great-again
-// @version         1.2.2
+// @icon            https://raw.githubusercontent.com/devunt/make-gis-great-again/master/icons/icon.png
+// @version         1.3.1
 // @description     This userscript adds "View Image" button to Google Image Search results.
 // @description:ru  Этот скрипт добавляет кнопку "Показать в полном размере" к результатам Google Image Search.
+// @description:sl  Ponovno prikaže gumb "Ogled slike" na Google Slikah.
+// @description:lt  Šis vartotojo skriptas prideda mygtuką "Rodyti vaizdą" į Google vaizdo paieškos rezultatus.
+// @description:pl  Ten skrypt przywraca przycisk "Pokaż Obraz" do wyszukiwarki obrazów Google
 // @author          Bae Junehyeon
 // @run-at          document-end
 // @include         http*://*.google.tld/search*tbm=isch*
+// @grant           GM_xmlhttpRequest
+// @connect         images.google.com
 // ==/UserScript==
 
 const lang = {
-  en: 'View Image',
-  ru: 'Показать в полном размере'
+  en: {
+    viewImage: 'View Image',
+    otherResolutions: 'Other resolutions'
+  },
+  ru: {
+    viewImage: 'Показать в полном размере',
+    otherResolutions: 'Все размеры'
+  },
+  uk: {viewImage: 'Показати зображення'},
+  ja: {viewImage: '画像を表示'},
+  he: {viewImage: 'הצג תמונה'},
+  fr: {viewImage: 'Voir l\'image'},
+  sl: {viewImage: 'Ogled slike'},
+  ar: {viewImage: 'عرض الصورة'},
+  de: {viewImage: 'Bild ansehen'},
+  pt: {viewImage: 'Ver imagem'},
+  lt: {viewImage: 'Rodyti vaizdą'},
+  pl: {viewImage: 'Pokaż Obraz'},
+  se: {viewImage: 'Visa bild'},
 };
 
-const localizedViewImage = lang[(lang[navigator.language] ? navigator.language : 'en')];
+function getLocalizedString(string) {
+  return lang[navigator.language][string] ?
+         lang[navigator.language][string] :
+         lang.en[string];
+}
 
-function addButton(node) {
+let linkToCurrentImage = '';
+
+function processElement(node) {
   if (node.nodeType === Node.ELEMENT_NODE) {
     if (node.classList.contains('irc_ris')) {
       let container = node.closest('.irc_c');
-      
+
       let similarImages = node.querySelectorAll('.rg_l');
-      similarImages.forEach((image) => {
+      [].forEach.call(similarImages, (image) => {
         image.addEventListener('click', updateLinkAfterClickOnSimilar);
       });
-      
+
       let thumbnail = document.querySelector('img[name="' + container.dataset.itemId + '"]');
       let meta = thumbnail.closest('.rg_bx').querySelector('.rg_meta');
 
       let metadata = JSON.parse(meta.innerHTML);
-      let src = metadata.ou;
+      linkToCurrentImage = metadata.ou;
 
       let buttons = container.querySelector('.irc_but_r tr');
 
-      let button = buttons.querySelector('td.mgisga');
+      let button = buttons.querySelector('td.mgisga_fullSize');
       if (button === null) {
-        let openButton = buttons.querySelector('td');
-
-        button = openButton.cloneNode(true);
-        button.classList.add('mgisga');
-        button.querySelector('._WKw').innerText = localizedViewImage;
-
-        let link = button.querySelector('a');
-        link.href = src;
-        link.className = '';
-        link.removeAttribute('data-cthref');
-        link.removeAttribute('jsaction');
-
-        openButton.after(button);
+        addButton(buttons, 'mgisga_fullSize', getLocalizedString('viewImage'), linkToCurrentImage);
+        addButton(buttons, 'mgisga_otherRes', getLocalizedString('otherResolutions'), searchOtherResolutions);
       }
 
       let link = button.querySelector('a');
       link.href = src;
     }
   }
+}
+
+/* buttonReaction may be string-url or function-callback */
+function addButton(buttonContainer, buttonClass, buttonText, buttonReaction) {
+  let openButton = buttonContainer.querySelector('td');
+
+  let button = openButton.cloneNode(true);
+  button.classList.add(buttonClass);
+  button.querySelector('._WKw').innerText = buttonText;
+
+  let link = button.querySelector('a');
+  link.className = '';
+  link.removeAttribute('data-cthref');
+  link.removeAttribute('jsaction');
+
+  if (typeof buttonReaction === 'string') {
+    link.href = buttonReaction;
+  } else {
+    link.removeAttribute('href');
+    button.addEventListener('click', buttonReaction);
+  }
+
+  return openButton.after(button);
+}
+
+function searchOtherResolutions() {
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://images.google.com/searchbyimage?&image_url=' + linkToCurrentImage,
+    onload: response=>{
+      let imgID = response.responseText.match(/simg:([^&]+)/)[1];
+      let tld = location.host.match(/\w+$/)[0];
+      location.href = 'https://www.google.' + tld + '/search?tbm=isch&tbs=simg:' + imgID;
+    }
+  });
 }
 
 function updateLinkAfterClickOnSimilar({target:node}) {
@@ -70,8 +125,8 @@ const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     const addedNodes = mutation.addedNodes || [];
 
-    addedNodes.forEach((newNode) => {
-        addButton(newNode);
+    [].forEach.call(addedNodes, (newNode) => {
+      processElement(newNode);
     });
   });
 });
@@ -80,5 +135,3 @@ observer.observe(document.body, {
   childList: true,
   subtree: true
 });
-
-addButton(document.body);
